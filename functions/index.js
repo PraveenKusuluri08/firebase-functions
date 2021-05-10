7055cd19b4e17e0f16f31d8395f8bbfc88c0ccfb
praveenkusuluri08 <praveenkusuluri07@gmail.com>
2021-05-10 20:29:02 +0530

    fb-functions-get

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,48 @@
-const functions = require("firebase-functions");
+const functions = require("firebase-functions")
+const admin = require("firebase-admin")
+admin.initializeApp()
+const express = require("express");
+const app =express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// exports.add =functions.https.onRequest((req,res)=>{
-//   console.log("ARRAY SAMPLE")
-//   var array=[1,4,54,21,41,14];
-//   console.log(array);
-//   var array1= array.filter(v =>v>=10)
-//   res.send(array1)
-// })
-exports.hello=functions.https.onRequest((req,res)=>{
-  console.log("Hello")
-  res.send("Hello")
+app.get("/data",(req,res)=>{
+  admin
+    .firestore()
+    .collection("data")
+    .get()
+    .then((data) => {
+      let value = []
+      data.forEach((doc) => {
+        value.push(doc.data())
+      })
+      return res.json(value)
+    })
+    .catch((error) => {
+      res.status(500).json({ error: `Something ${error} went wrong` })
+    })
 })
+exports.dataCreate = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: `Bad request` })
+  } else {
+    const dataFields = {
+      autherFirstName: req.body.autherFirstName,
+      autherLastName: req.body.autherLastName,
+      autherId: req.body.autherId,
+      createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    }
+    admin
+      .firestore()
+      .collection("data")
+      .add(dataFields)
+      .then((doc) => {
+        res.json({ message: `Document ${doc.id} is created successfully` })
+      })
+      .catch((error) => {
+        res.status(500).json({ error: `Document is failed to create` })
+      })
+  }
+})
+
+//https://baseurl.com/api/
+
+exports.api = functions.https.onRequest(app)
